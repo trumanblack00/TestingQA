76a17f9788fb838592f929536f1cc54c2eeb1ad9
Gabriela Paternoste <121585938+trumanblack00@users.noreply.github.com>
2023-10-27 16:23:09 -0300

    Create 1Cypress.practica.cypress.assertions.spec.js

---
+++ 1Cypress.practica.cypress.assertions.spec.js
@@ -0,0 +1,19 @@
+// practica.cypress.aserrtions.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+describe('My first assertions practice', () => {
+    it('Automate a form filling', () => {
+      cy.visit('https://qavalidation.com/demo-form')
+
+      //cy.get("#g4072-fullname").type('Falsename')
+
+      /*cy.get("input[name=Password]").type('pwd').should('have.value', 'pwd')*/
+
+      /*cy.get("#login").click()*/
+
+
+    })
+  })
